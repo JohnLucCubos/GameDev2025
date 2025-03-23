29557509e62747718f5de835273a22af86444601
Isayah <isayahweedon@gmail.com>
2025-03-23 01:17:22 -0700

    Scene and Gameplay Structure initiation,
    Set up the first layout of the color matching game. Created the color manager and the the color utilities file

---
+++ Game Demo 2025/Assets/Scripts/Gameplay/ColorMatch/ColorConstants.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+public struct GameColors
+{
+    string colorName;
+    Color color;
+    public GameColors(string a, Color b)
+    {
+        colorName = a; 
+        color = b;
+    }
+}
+
+// Color data 
