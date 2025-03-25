53a0512f00fe68689c1086f7b5bfc1c74bd2277a
Isayah <isayahweedon@gmail.com>
2025-03-24 21:47:27 -0700

    Fixed Errors and Reorganized GameColor Struct
    Fixed the errors with the GameColors by deleting the script and moving the definition of the GameColors struct to the ColorGameManager script. Made the struct serializable and created the array so that colors can be created in the inspector.

--- Game Demo 2025/Assets/Scripts/Managers/ColorGameManager.cs
+++ Game Demo 2025/Assets/Scripts/Managers/ColorGameManager.cs
@@ -2,6 +2,7 @@ using UnityEngine;
 
 public class ColorGameManager : MonoBehaviour
 {
+    public GameColors[] colorArray;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -12,5 +13,18 @@ public class ColorGameManager : MonoBehaviour
     void Update()
     {
         
+    }
+}
+
+[System.Serializable]
+public struct GameColors
+{
+     public string colorName;
+     public Color color;
+
+    public GameColors(string name, Color c)
+    {
+        colorName = name;
+        color = c;
     }
 }
